48b09b31ca161c8e357d04a01143b57ee23c8a14
elsa2025s <elsasnowpro@gmail.com>
2025-07-01 14:10:54 +0200

    Add files via upload

---
+++ Page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Bonjour depuis l'export statique !</h1>
+      <p>Si tu vois ceci, index.html a été généré avec succès.</p>
+    </div>
+  );
+}
